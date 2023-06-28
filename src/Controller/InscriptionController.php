f90965b7e12530ebfde2686f80eba9b2dfd4486d
michelbdg <114735296+michelbdg@users.noreply.github.com>
2023-06-28 11:40:53 +0200

    update

---
+++ src/Controller/InscriptionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\InscriptionType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InscriptionController extends AbstractController
+{
+    #[Route('/inscription', name: 'app_inscription')]
+    public function index(): Response
+    {
+        $user = new User();
+        $form = $this->createForm(InscriptionType::class,$user);
+        return $this->render('inscription/Inscription.html.twig', [
+            'formInscription' => $form->createView()
+        ]);
+    }
+}
